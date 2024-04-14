7fdd2ec3a170f513914e590b0a1be4d069c39af8
Smmook <diegobix97@gmail.com>
2024-04-14 18:08:26 +0200

    local storage fix

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -2,7 +2,7 @@ import { PageProps } from "$fresh/server.ts";
 import Navbar from "../components/Navbar.tsx";
 
 export default ({ Component }: PageProps) => {
-  const logged = window.localStorage.getItem("name") !== null;
+  const logged = localStorage.getItem("name") !== null;
   return (
     <>
       <Navbar logged={logged} />
